d4034f614de549942543f32bf521288bfb234c12
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-06-01 10:53:02 +0600

    registration form done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -49,9 +50,35 @@ const Navbar = () => {
                                 <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
                                 </a>
                             </li>
-                            <li className='max-lg:border-b max-lg:py-3'><a href='javascript:void(0)'
-                                className='hover:text-blue-600 text-[15px] font-bold text-blue-600 block'>Home</a></li>
-                            <li className='group max-lg:border-b max-lg:py-3 relative'>
+
+
+
+
+                            {/* navbar all li here  */}
+
+                            <li className='max-lg:border-b max-lg:py-3'>
+                                <NavLink to={'/'} className={({ isActive }) => isActive ? "hover:text-blue-600 text-[15px] font-bold text-blue-600 block" : "hover:text-[#007bff] text-gray-600 text-[15px] font-bold block"}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className='max-lg:border-b max-lg:py-3'>
+                                <NavLink to={'/registration'} className={({ isActive }) => isActive ? "hover:text-blue-600 text-[15px] font-bold text-blue-600 block" : "hover:text-[#007bff] text-gray-600 text-[15px] font-bold block"}>
+
+                                </NavLink>
+                            </li>
+
+
+
+
+
+
+
+
+
+
+
+                            {/* for dropdown menu  */}
+                            {/* <li className='group max-lg:border-b max-lg:py-3 relative'>
                                 <a href='javascript:void(0)'
                                     className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold lg:hover:fill-[#007bff] block'>Pages<svg
                                         xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" className="ml-1 inline-block"
@@ -74,37 +101,12 @@ const Navbar = () => {
                                     <li className='border-b py-2 '><a href='javascript:void(0)'
                                         className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Blog</a></li>
                                 </ul>
-                            </li>
-                            <li className='group max-lg:border-b max-lg:py-3 relative'>
-                                <a href='javascript:void(0)'
-                                    className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold lg:hover:fill-[#007bff] block'>Blog<svg
-                                        xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" className="ml-1 inline-block"
-                                        viewBox="0 0 24 24">
-                                        <path
-                                            d="M12 16a1 1 0 0 1-.71-.29l-6-6a1 1 0 0 1 1.42-1.42l5.29 5.3 5.29-5.29a1 1 0 0 1 1.41 1.41l-6 6a1 1 0 0 1-.7.29z"
-                                            data-name="16" data-original="#000000" />
-                                    </svg>
-                                </a>
-                                <ul
-                                    className='absolute shadow-lg bg-white space-y-3 lg:top-5 max-lg:top-8 -left-6 min-w-[250px] z-50 max-h-0 overflow-hidden group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500'>
-                                    <li className='border-b py-2 '><a href='javascript:void(0)'
-                                        className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Foods</a></li>
-                                    <li className='border-b py-2 '>
-                                        <a href='javascript:void(0)'
-                                            className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Sale</a>
-                                    </li>
-                                    <li className='border-b py-2 '><a href='javascript:void(0)'
-                                        className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Marketing</a></li>
-                                    <li className='border-b py-2 '><a href='javascript:void(0)'
-                                        className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Investment</a></li>
-                                </ul>
-                            </li>
-                            <li className='max-lg:border-b max-lg:py-3'><a href='javascript:void(0)'
-                                className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Team</a></li>
-                            <li className='max-lg:border-b max-lg:py-3'><a href='javascript:void(0)'
-                                className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>About</a></li>
-                            <li className='max-lg:border-b max-lg:py-3'><a href='javascript:void(0)'
-                                className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Contact</a></li>
+                            </li> */}
+
+                            {/* <li className='max-lg:border-b max-lg:py-3'><a href='javascript:void(0)'
+                                className='hover:text-[#007bff] text-gray-600 text-[15px] font-bold block'>Team</a>
+                            </li> */}
+
                         </ul>
                     </div>
 
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -5,7 +5,16 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
-        <div className=" max-w-[1920px] lg:w-[90%] w-[98%] mx-auto">
+        <div className=" max-w-[1920px] lg:w-[90%] w-[98%] mx-auto font-mont"
+            style={{
+                // backgroundImage: `url(${background})`,
+                // backgroundColor: "#F0F0F0",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+
+                width: "100%",
+            }}>
             <Navbar />
             <div className="min-h-[calc(100vh-520.667px)]">
                 <Outlet></Outlet>
