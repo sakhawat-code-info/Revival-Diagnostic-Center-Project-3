9dc0b967b25bfbc1153bd5eee9da48c86eb0d873
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-06-01 17:16:59 +0600

    Footer section done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,10 @@
-
+import logo from '../assets/logo.png'
 
 
 // bg-teal-800 
 // bg-[#382335]
+
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -11,10 +13,14 @@ const Footer = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
 
                     <div className="lg:col-span-2">
-                        <a href='javascript:void(0)'><img src="https://readymadeui.com/readymadeui-white.svg" alt="logo"
-                            className='w-44 mb-8' /></a>
-                        <p className="text-gray-300 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
+                        <Link to={'/'} className="flex items-center gap-1 text-white">
+                            <img src={logo} alt="logo" className='w-14' />
+                            <h2 className="text-xl font-extrabold text-white inline-block">Revival Hospital</h2>
+                        </Link>
+
+                        <p className="text-gray-300 text-sm">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae optio quia labore ad deleniti quae fugiat commodi, nihil corporis nulla itaque sint corrupti ipsa adipisci facilis? Expedita qui id assumenda?
+                        </p>
                     </div>
 
                     <div>
@@ -34,16 +40,16 @@ const Footer = () => {
                             <li><a href="javascript:void(0)">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="fill-gray-300 inline hover:fill-white w-6 h-6"
                                     viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd"
+                                    <path
                                         d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7v-7h-2v-3h2V8.5A3.5 3.5 0 0 1 15.5 5H18v3h-2a1 1 0 0 0-1 1v2h3v3h-3v7h4a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z"
-                                        clip-rule="evenodd" />
+                                    />
                                 </svg></a></li>
                             <li><a href="javascript:void(0)" className="text-gray-300 hover:text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="fill-gray-300 inline hover:fill-white w-6 h-6"
                                     viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd"
+                                    <path
                                         d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-2.5 8.2v5.3h-2.79v-4.93a1.4 1.4 0 0 0-1.4-1.4c-.77 0-1.39.63-1.39 1.4v4.93h-2.79v-8.37h2.79v1.11c.48-.78 1.47-1.3 2.32-1.3 1.8 0 3.26 1.46 3.26 3.26zM6.88 8.56a1.686 1.686 0 0 0 0-3.37 1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 1.57v8.37H5.5v-8.37h2.77z"
-                                        clip-rule="evenodd" />
+                                    />
                                 </svg></a></li>
                             <li><a href="javascript:void(0)" className="text-gray-300 hover:text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="fill-gray-300 inline hover:fill-white w-6 h-6"
@@ -64,9 +70,9 @@ const Footer = () => {
                     <div>
                         <h4 className="text-lg font-semibold mb-8 text-white">Contact Us</h4>
                         <div className="space-y-4">
-                            <p className="text-gray-300 text-sm">123 Street, City Name</p>
-                            <p className="text-gray-300 text-sm">contact@example.com</p>
-                            <p className="text-gray-300 text-sm">+1 234 567 890</p>
+                            <p className="text-gray-300 text-sm">123 Street, Dhaka</p>
+                            <p className="text-gray-300 text-sm">sakhawat.code.info@gmail.com</p>
+                            <p className="text-gray-300 text-sm">+88 01939-727303</p>
                         </div>
                     </div>
 
@@ -74,8 +80,8 @@ const Footer = () => {
             </div>
 
             <div className="text-center py-5 bg-[#08355e]">
-                <p className='text-gray-300 text-sm'>© 2023<a href='https://readymadeui.com/' target='_blank'
-                    className="hover:underline mx-1">ReadymadeUI</a>All Rights Reserved.</p>
+                <p className='text-gray-300 text-sm'>© 2024<a href='https://readymadeui.com/' target='_blank'
+                    className="hover:underline mx-1">Revival Hospital</a>All Rights Reserved.</p>
             </div>
         </footer>
     );
